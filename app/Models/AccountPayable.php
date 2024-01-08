7ac04fa2e499e3bbcc84a9328b4beedca88c5b59
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2024-01-08 15:46:52 +0530

    change return type

--- app/Models/AccountPayable.php
+++ app/Models/AccountPayable.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AccountPayable extends Model
 {
     use HasFactory;
+    protected $connection = 'student_service';
     protected $fillable = ['student_id', 'amount', 'type', 'eligibility', 'status'];
 }
--- app/Models/StudentPayment.php
+++ app/Models/StudentPayment.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class StudentPayment extends Model
 {
     use HasFactory;
+    protected $connection = 'student_service';
 }
